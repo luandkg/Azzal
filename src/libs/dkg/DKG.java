9d3686c9cbeaedc625dd8c79c0cccb58ae760626
Luan Freitas <luandkg@gmail.com>
2023-12-09 10:21:06 -0300

    :sparkles: Unificando Parser DKG e Serializador DKG

--- src/libs/dkg/DKG.java
+++ src/libs/dkg/DKG.java
@@ -8,6 +8,10 @@ import java.io.*;
 
 public class DKG {
 
+    // 2023 12 09 - MIGRAR ARRAYLIST -> LISTA
+    // 2023 12 09 - UNIR PARSER E SERIALIZADOR
+
+
     private Lista<DKGObjeto> mDKGObjetos;
 
     public DKG() {
@@ -55,41 +59,25 @@ public class DKG {
     // IO
 
     public void abrir(String eArquivo) {
-        ParserDKG parserDKGC = new ParserDKG();
-        parserDKGC.parse(arquivo_ler(eArquivo), this);
+        parse(arquivo_ler(eArquivo), this);
     }
 
 
     public void parser(String eTexto) {
-        ParserDKG parserDKGC = new ParserDKG();
-        parserDKGC.parse(eTexto, this);
+        parse(eTexto, this);
     }
 
     public void salvar(String eArquivo) {
-
-        EscritorDKG escritor = new EscritorDKG();
-        escritor.montar("", mDKGObjetos);
-
-        arquivo_escrever(eArquivo, escritor.getTexto());
+        arquivo_escrever(eArquivo, serializar(mDKGObjetos));
     }
 
 
     public String toString() {
-
-        EscritorDKG escritor = new EscritorDKG();
-
-        escritor.montar("", mDKGObjetos);
-
-        return escritor.getTexto();
+        return serializar(mDKGObjetos);
     }
 
     public String toDocumento() {
-
-        EscritorDKG escritor = new EscritorDKG();
-
-        escritor.montar("", mDKGObjetos);
-
-        return escritor.getTexto();
+        return serializar(mDKGObjetos);
     }
 
     // OBJETO
@@ -239,4 +227,371 @@ public class DKG {
         return lista;
     }
 
+
+
+    // PARSER TEXTUM
+    private final String ALFABETO_INICIAL = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ_@.123456789";
+    private final String ALFABETO_FINAL = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ_@.0123456789";
+
+    private String mTextoParser;
+    private int mIndex;
+    private int mMaximo;
+
+    private Lista<String> mParserErros;
+
+
+
+    public Lista<String> getErros() {
+        return mParserErros;
+    }
+
+
+
+    // PARSER
+
+    private void parse(String eDocumento, DKG eEmpacotador) {
+
+        mTextoParser = "";
+        mIndex = 0;
+        mMaximo = 0;
+        mParserErros = new Lista<String>();
+
+        eEmpacotador.getObjetos().limpar();
+
+        // mPacotes = new ArrayList<Pacote>();
+
+        mTextoParser = eDocumento;
+        mIndex = 0;
+        mMaximo = eDocumento.length();
+
+        //   String eAnterior = "";
+
+        while (mIndex < mMaximo) {
+            String l = String.valueOf(eDocumento.charAt(mIndex));
+
+            if (l.contentEquals(" ")) {
+            } else if (l.contentEquals("\n")) {
+            } else if (l.contentEquals("\t")) {
+            } else if (l.contentEquals("\r")) {
+
+            } else if (l.contentEquals("!")) {
+
+                mIndex += 1;
+                parserObjeto(eEmpacotador.getObjetos());
+
+            } else {
+
+            }
+
+            mIndex += 1;
+        }
+
+    }
+
+    private String obterPalavra() {
+
+        String p = String.valueOf(mTextoParser.charAt(mIndex));
+        String ret = p;
+
+        if (ALFABETO_INICIAL.contains(p)) {
+
+            mIndex += 1;
+
+            while (mIndex < mMaximo) {
+
+                String d = String.valueOf(mTextoParser.charAt(mIndex));
+
+                if (ALFABETO_FINAL.contains(d)) {
+                    ret += d;
+                } else {
+                    mIndex -= 1;
+                    break;
+                }
+
+                mIndex += 1;
+
+            }
+
+        }
+
+        return ret;
+    }
+
+
+    private String esperarPalavra() {
+
+        // System.out.println("Aqui " + String.valueOf(mDocumento.charAt(mIndex)));
+
+        String ret = "";
+
+        while (mIndex < mMaximo) {
+
+            String l = String.valueOf(mTextoParser.charAt(mIndex));
+
+            if (ALFABETO_INICIAL.contains(l)) {
+                ret = obterPalavra();
+                break;
+
+            } else if (l.contentEquals(" ")) {
+            } else if (l.contentEquals("\n")) {
+            } else if (l.contentEquals("\t")) {
+
+            } else {
+
+            }
+
+            mIndex += 1;
+
+        }
+
+        return ret;
+    }
+
+    private boolean EsperarPor(String Esperando) {
+
+        // System.out.println("Aqui " + String.valueOf(mDocumento.charAt(mIndex)));
+
+        boolean ret = false;
+
+        while (mIndex < mMaximo) {
+
+            String l = String.valueOf(mTextoParser.charAt(mIndex));
+
+            if (Esperando.contentEquals(l)) {
+                ret = true;
+                mIndex += 1;
+                break;
+
+            } else if (l.contentEquals(" ")) {
+            } else if (l.contentEquals("\n")) {
+            } else if (l.contentEquals("\t")) {
+
+            } else {
+
+            }
+
+            mIndex += 1;
+
+        }
+
+        return ret;
+    }
+
+    private void parserObjeto(Lista<DKGObjeto> lsPacotes) {
+
+        String Palavra = decodifica(esperarPalavra());
+
+        DKGObjeto NovoPacote = new DKGObjeto(Palavra);
+        lsPacotes.adicionar(NovoPacote);
+
+        boolean esperar_dp1 = EsperarPor(":");
+        boolean esperar_dp2 = EsperarPor(":");
+        boolean esperar_abrir = EsperarPor("{");
+
+        if (esperar_dp1 && esperar_dp2 && esperar_abrir) {
+
+            dentroObjeto(NovoPacote);
+
+        } else {
+            mParserErros.adicionar("ERRO : Era esperado :: { ");
+
+        }
+
+    }
+
+    private void dentroObjeto(DKGObjeto eDKGObjetoCorrente) {
+
+        while (mIndex < mMaximo) {
+            String l = String.valueOf(mTextoParser.charAt(mIndex));
+
+            if (l.contentEquals(" ")) {
+            } else if (l.contentEquals("\n")) {
+            } else if (l.contentEquals("\t")) {
+
+            } else if (l.contentEquals("!")) {
+
+                mIndex += 1;
+                parserObjeto(eDKGObjetoCorrente.getObjetos());
+
+            } else if (l.contentEquals("@")) {
+
+                mIndex += 1;
+                parserAtributo(eDKGObjetoCorrente.getAtributos());
+
+            } else if (l.contentEquals("}")) {
+
+                break;
+
+            } else {
+                mParserErros.adicionar(mIndex + " : " + l);
+
+            }
+
+            mIndex += 1;
+        }
+
+    }
+
+
+    private String esperarTexto() {
+
+        // System.out.println("Aqui " + String.valueOf(mDocumento.charAt(mIndex)));
+
+        String ret = "";
+
+        while (mIndex < mMaximo) {
+
+            String l = String.valueOf(mTextoParser.charAt(mIndex));
+
+            if (l.contentEquals("'")) {
+                mIndex += 1;
+
+                ret = buscandoTexto("'");
+                break;
+            } else if (l.contentEquals("\"")) {
+                mIndex += 1;
+
+                ret = buscandoTexto("\"");
+                break;
+            } else if (l.contentEquals(" ")) {
+            } else if (l.contentEquals("\n")) {
+            } else if (l.contentEquals("\t")) {
+
+            } else {
+
+            }
+
+            mIndex += 1;
+
+        }
+
+        return ret;
+    }
+
+    private String buscandoTexto(String Finalizador) {
+
+        // System.out.println("Aqui " + String.valueOf(mDocumento.charAt(mIndex)));
+
+        String ret = "";
+
+        while (mIndex < mMaximo) {
+
+            String l = String.valueOf(mTextoParser.charAt(mIndex));
+
+            if (l.contentEquals(Finalizador)) {
+                mIndex += 1;
+                break;
+
+            } else {
+                ret += l;
+            }
+
+            mIndex += 1;
+
+        }
+
+        return ret;
+    }
+
+    private void parserAtributo(Lista<DKGAtributo> ls_Identificadores) {
+
+        String NomeIdentificador = decodifica(esperarPalavra());
+
+        DKGAtributo IDC = new DKGAtributo(NomeIdentificador);
+        ls_Identificadores.adicionar(IDC);
+
+        if (EsperarPor("=")) {
+            // System.out.println("Achou =");
+
+            String eValor = esperarTexto();
+            IDC.setValor(decodifica(eValor));
+            // Parser_DentroPacote(NovoPacote);
+
+        } else {
+            System.out.println("ERRO : Era esperado =");
+
+        }
+
+    }
+
+    public static String decodifica(String e) {
+        e = e.replace("_H", "-");
+        e = e.replace("_D", "\"");
+        e = e.replace("_S", "'");
+        e = e.replace("_A", "@");
+        e = e.replace("_U", "_");
+
+        return e;
+    }
+
+
+
+
+
+    // SERIALIZAR
+
+
+    private String mSerializando = "";
+
+
+    public String serializar(Lista<DKGObjeto> ls_objetos){
+        mSerializando = "";
+        serializar_objetos("", ls_objetos);
+        return mSerializando;
+    }
+
+    public void serializando_adicionarLinha(String eLinha) {
+        mSerializando += eLinha + "\n";
+    }
+
+
+
+    private void serializar_objetos(String ePrefixo, Lista<DKGObjeto> ls_objetos) {
+
+        for (DKGObjeto PacoteC : ls_objetos) {
+
+            if (PacoteC.getObjetos().getQuantidade() == 0 && PacoteC.getAtributos().getQuantidade() == 0) {
+
+                serializando_adicionarLinha(ePrefixo + "!" + codifica(PacoteC.getNome()) + " :: { } ");
+
+            } else if (PacoteC.getObjetos().getQuantidade() == 0 && PacoteC.getAtributos().getQuantidade() > 0) {
+
+                String eIdentificadores = "";
+
+                for (DKGAtributo IdentificadorC : PacoteC.getAtributos()) {
+                    eIdentificadores += " @" + codifica(IdentificadorC.getNome()) + " = " + "\"" + codifica(IdentificadorC.getValor()) + "\"";
+                }
+
+                serializando_adicionarLinha(ePrefixo + "!" + codifica(PacoteC.getNome()) + " :: { " + eIdentificadores + " } ");
+
+            } else if (PacoteC.getObjetos().getQuantidade() > 0) {
+
+                serializando_adicionarLinha(ePrefixo + "!" + codifica(PacoteC.getNome()) + " :: { ");
+
+                for (DKGAtributo IdentificadorC : PacoteC.getAtributos()) {
+                    serializando_adicionarLinha(ePrefixo+"  @" + codifica(IdentificadorC.getNome()) + " = " + "\"" + codifica(IdentificadorC.getValor()) + "\"");
+                }
+
+                serializar_objetos(ePrefixo + "  ", PacoteC.getObjetos());
+
+                serializando_adicionarLinha(ePrefixo+ "}");
+
+            }
+
+
+        }
+
+    }
+
+
+    public static String codifica(String e) {
+
+        e = e.replace("_", "_U");
+        e = e.replace("@", "_A");
+        e = e.replace("'", "_S");
+        e = e.replace("\"", "_D");
+        e = e.replace("-", "_H");
+
+        return e;
+    }
 }
