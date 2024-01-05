8611ab191f298e5bdcbd9864f42028f717b1bd13
Luan Freitas <luandkg@gmail.com>
2024-01-05 15:22:02 -0300

    :sparkles: Comparador de Projeto remoto e local

--- src/libs/luan/Strings.java
+++ src/libs/luan/Strings.java
@@ -269,4 +269,24 @@ public class Strings {
         };
     }
 
+    public static Lista<String> ORDENAR(Lista<String> entradas) {
+
+        int n = entradas.getQuantidade();
+        String temp = null;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 1; j < (n - i); j++) {
+
+                if (entradas.get(j - 1).compareTo(entradas.get(j)) > 0) {
+                    temp = entradas.get(j - 1);
+                    entradas.set(j - 1, entradas.get(j));
+                    entradas.set(j, temp);
+
+                }
+
+            }
+        }
+        return entradas;
+    }
+
 }
