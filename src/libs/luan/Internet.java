71c819495136805642aaf5240211b6fd2827b3e3
Luan Freitas <luandkg@gmail.com>
2024-11-08 09:24:13 -0300

    :sparkles:Funções para realizar uma requisição na internet utilizando como resposta as Classes Resultado e Transformação do Resultado com parser de JSON para DKG.

--- src/libs/luan/Internet.java
+++ src/libs/luan/Internet.java
@@ -6,8 +6,13 @@ import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.ProtocolException;
 import java.net.URL;
+import java.nio.charset.StandardCharsets;
+
 import libs.arquivos.binario.ArenaChunk;
 import libs.arquivos.binario.ByteChunk;
+import libs.dkg.DKG;
+import libs.dkg.DKGObjeto;
+import libs.oo.JSON;
 
 public class Internet {
 
@@ -63,5 +68,47 @@ public class Internet {
 
     }
 
+    public static Resultado<String, String> http(String webpage) {
+
+        URL url = null;
+
+        try {
+            url = new URL(webpage);
+        } catch (MalformedURLException e) {
+            return Resultado.FALHAR("PROBLEMA_CONEXAO");
+        }
+
+        try {
+            HttpURLConnection huc = (HttpURLConnection) url.openConnection();
+            HttpURLConnection.setFollowRedirects(false);
+            huc.setConnectTimeout(15 * 1000);
+            huc.setRequestMethod("GET");
+            huc.setRequestProperty("User-Agent", "Mozilla/5.0 (Windows; U; Windows NT 6.0; en-US; rv:1.9.1.2) Gecko/20090729 Firefox/3.5.2 (.NET CLR 3.5.30729)");
+            huc.connect();
+            InputStream input = huc.getInputStream();
+
+            String json = new String(input.readAllBytes(), StandardCharsets.UTF_8);
+
+            byte[] bytes = json.getBytes("UTF-8");
+            return Resultado.OK(new String(bytes, StandardCharsets.UTF_8));
+
+        } catch (IOException e) {
+            return Resultado.FALHAR("PROBLEMA_CONEXAO");
+        }
+
+
+    }
+
+
+    public static Resultado<DKGObjeto, String> http_dkg_objeto(String link_url) {
+
+        Resultado<String, String> dados = Internet.http(link_url);
+        if (dados.isOK()) {
+            DKG documento_dkg = JSON.JSON_TO_DKG(dados.getOK());
+            return Resultado.OK(documento_dkg.unicoObjeto("JSON"));
+        }
+
+        return Resultado.FALHAR(dados.getErro());
+    }
 
 }
