6d9b317a8eb8cbdeff924949f2669dd22ab2cb49
Luan Freitas <luandkg@gmail.com>
2024-11-23 12:14:25 -0300

    :hammer: Agora é possível ler o ArquivoAtzumTronarko criado pelo ServicoTronarkoExportador !

---
+++ src/apps/app_atzum/utils/ArquivoAtzumTronarko.java
@@ -0,0 +1,41 @@
+package apps.app_atzum.utils;
+
+import apps.app_atzum.AtzumCreator;
+import libs.arquivos.DSInterno;
+import libs.arquivos.ds.DS;
+import libs.arquivos.ds.DSIndexador;
+import libs.arquivos.ds.DSItem;
+import libs.entt.Entidade;
+import libs.luan.Lista;
+import libs.luan.Opcional;
+import libs.luan.fmt;
+
+public class ArquivoAtzumTronarko {
+
+    private Lista<DSItem> mIndice;
+
+    public ArquivoAtzumTronarko(String eTronarko) {
+        String arquivo_atzum = AtzumCreator.LOCAL_GET_ARQUIVO("tronarkos/atzum_tronarko_" + eTronarko + ".ds");
+
+        //  DS.DUMP_TABELA(arquivo_atzum);
+
+        Opcional<DSItem> op_init = DS.buscar_item(arquivo_atzum, "@AtzumTronarko.index");
+
+        if (Opcional.IS_OK(op_init)) {
+            mIndice = DSIndexador.GET_INDEX(arquivo_atzum, "@AtzumTronarko.index");
+
+
+
+            DS.DUMP_ITENS(mIndice);
+        }
+
+
+    }
+
+    public Lista<Entidade> getCidadesDadosPublicados() {
+        Lista<Entidade> mCidadesDadosPublicados = DSInterno.ENTT_ABRIR(DSIndexador.GET_ITEM(mIndice, "@dados/tronarko_cidades_dados_publicados.entts").get());
+        return mCidadesDadosPublicados;
+    }
+
+
+}
