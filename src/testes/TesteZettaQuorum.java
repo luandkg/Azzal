ada43426ff5f6d52d58acb5120efe9df1957e910
Luan Freitas <luandkg@gmail.com>
2024-11-23 10:10:56 -0300

    :rotating_light: Organiza os testes para replicação de ZettaColecao

--- src/testes/TesteZettaQuorum.java
+++ src/testes/TesteZettaQuorum.java
@@ -191,6 +191,8 @@ public class TesteZettaQuorum {
         }
 
         //tronarkum.exibir_colecao();
+
+        zeta.fechar();
     }
 
     public static void ver_estrutura() {
@@ -242,7 +244,7 @@ public class TesteZettaQuorum {
 
             item.get().at("ResumoFTS", FTS.PARSER_TO_DOCUMENTO(item.get().at("Resumo")));
 
-           //    item.atualizar();
+            //    item.atualizar();
         }
 
         // fts.exibir_colecao();
@@ -250,6 +252,8 @@ public class TesteZettaQuorum {
         fts.exibir_colecao(0, 10);
 
         PortuguesFTS portugues = new PortuguesFTS();
+
+        Lista<Entidade> fts_analisado = new Lista<Entidade>();
 
         for (Entidade item : fts.getItens()) {
 
@@ -259,22 +263,271 @@ public class TesteZettaQuorum {
 
             Lista<Entidade> fts_dados = FTS.GET_DOCUMENTO(item.at("ResumoFTS"));
 
-            FTS.RETIRAR_PALAVRAS(fts_dados,portugues.getArtigos());
-            FTS.RETIRAR_PALAVRAS(fts_dados,portugues.getPreposicoes());
-            FTS.RETIRAR_PALAVRAS(fts_dados,portugues.getPronomes());
-            FTS.RETIRAR_PALAVRAS(fts_dados,portugues.getPronomesPossessivos());
+            FTS.RETIRAR_PALAVRAS(fts_dados, portugues.getArtigos());
+            FTS.RETIRAR_PALAVRAS(fts_dados, portugues.getPreposicoes());
+            FTS.RETIRAR_PALAVRAS(fts_dados, portugues.getPronomes());
+            FTS.RETIRAR_PALAVRAS(fts_dados, portugues.getPronomesPossessivos());
 
             FTS.ORDENAR_MAIOR_RANKING(fts_dados);
 
             ENTT.EXIBIR_TABELA(ENTT.SLICE(fts_dados, 0, 10));
 
+            FTS.MERGE(fts_analisado, ENTT.SLICE(fts_dados, 0, 10));
 
         }
 
         zeta.fechar();
 
+        ENTT.EXIBIR_TABELA(fts_analisado);
+
+        FTS.ORDENAR_MAIOR_RANKING(fts_analisado);
+        // FTS.ORDENAR_MAIOR_CITACAO(fts_analisado);
+
+        ENTT.EXIBIR_TABELA(ENTT.SLICE(fts_analisado, 0, 10));
+
 
     }
 
+    public static void init_replicacao() {
+
+        fmt.print("----------------- ZETA QUORUM :: REPLICACAO ------------------");
+
+        String arquivo_zeta = "/home/luan/assets/teste_fazendas/zeta.az";
+
+        ZettaQuorum zeta = new ZettaQuorum(arquivo_zeta);
+
+
+        ZettaColecao alpha = zeta.getColecaoSempre("@Alpha");
+        ZettaColecao beta = zeta.getColecaoSempre("@Beta");
+
+        ZettaColecao zeta_logs = zeta.getColecaoSempre("@Zeta::Logs");
+
+        // alpha.zerar();
+        // beta.zerar();
+        //zeta_logs.zerar();
+
+        fmt.print("++ Alpha Contagem : {}", alpha.contagem());
+        fmt.print("++ Beta  Contagem : {}", beta.contagem());
+
+        if (Aleatorio.aleatorio(100) > 60) {
+            int novos_alpha = Aleatorio.aleatorio_entre(2, 5);
+            for (int i = 0; i < novos_alpha; i++) {
+
+                Entidade novo = new Entidade();
+                novo.at("Valor", Aleatorio.aleatorio(100));
+                novo.at("Nome", PessoaNomeadorDeAkkax.get());
+                novo.at("TTC", Tronarko.getTronAgora().getTextoZerado());
+
+                alpha.adicionar(novo);
+            }
+        }
+
+        if (Aleatorio.aleatorio(100) > 50) {
+
+            for (ItemColecionavel item : alpha.getItensEditaveis()) {
+                if (Aleatorio.aleatorio(100) > 50) {
+
+                    item.get().at("Atualizacoes", item.get().atIntOuPadrao("Atualizacoes", 0) + 1);
+                    item.get().at("TTA", Tronarko.getTronAgora().getTextoZerado());
+                    item.get().at("Anterior", item.get().at("Valor"));
+                    item.get().at("Valor", Aleatorio.aleatorio(100));
+
+                    item.atualizar();
+                }
+            }
+
+        }
+
+        alpha.exibir_colecao();
+
+        fmt.print("Removendo de alfa");
+
+        if (Aleatorio.aleatorio(100) > 60) {
+
+            for (ItemColecionavel item : alpha.getItensEditaveis()) {
+                if (Aleatorio.aleatorio(100) > 70) {
+                    item.remover();
+                }
+            }
+
+        }
+
+        alpha.exibir_colecao();
+        beta.exibir_colecao();
+
+
+        fmt.print(">> OP : SIMETRIA");
+
+        Lista<Entidade> dados_primarios = alpha.getItens();
+        Lista<Entidade> dados_secundarios = beta.getItens();
+
+
+        fmt.print(">> OP : SIMETRIA :: NOVOS");
+
+
+        Lista<Entidade> novos = ENTT.ANALISAR_OBTER_NOVOS(dados_primarios, dados_secundarios, "@ID", "@RefID");
+
+        fmt.print("Quantidade : {}", ENTT.CONTAGEM(novos));
+
+        for (Entidade novo : novos) {
+            novo.at("@RefID", novo.at("@ID"));
+            novo.at("@Inserido", Tronarko.getTronAgora().getTextoZerado());
+        }
+
+        ENTT.ATRIBUTO_REMOVER(novos, "@ID");
+        ENTT.ATRIBUTO_REMOVER(novos, "@PTR");
+
+        ENTT.ATRIBUTO_TORNAR_PRIMEIRO(novos, "@RefID");
+
+        ENTT.EXIBIR_TABELA_COM_NOME(novos, "SIMETRIA -- NOVOS");
+
+        //tronarkum.exibir_colecao();
+
+
+        beta.adicionar_varios(novos);
+
+        if(novos.getQuantidade()>0){
+            Entidade log = new Entidade();
+            log.at("Tron",Tronarko.getTronAgora().getTextoZerado());
+            log.at("Status","Adicionando");
+            log.at("Quantidade",ENTT.CONTAGEM(novos));
+
+            zeta_logs.adicionar(log);
+        }
+
+        beta.exibir_colecao();
+
+
+        fmt.print(">> OP : SIMETRIA :: EXCLUIDOS");
+        Lista<Entidade> excluidos = ENTT.ANALISAR_OBTER_EXCLUIDOS(dados_primarios, dados_secundarios, "@ID", "@RefID");
+
+//ENTT.REMOVER_ITEM_SE_TIVER_ATRIBUTO(excluidos,"@Removido");
+
+        fmt.print("Quantidade : {}", ENTT.CONTAGEM(excluidos));
+
+        ENTT.EXIBIR_TABELA_COM_NOME(excluidos, "SIMETRIA -- EXCLUIDOS");
+
+
+        Lista<ItemColecionavel> itens_atualizaveis = beta.getItensEditaveis();
+
+        for (Entidade e : excluidos) {
+            //  beta.procurar_item_por_indice(e.atLong("@RefID"))
+
+            fmt.print("Procurar para remover : {}", e.at("@RefID"));
+
+            for (ItemColecionavel item : itens_atualizaveis) {
+                if (!item.isRemovido() && item.get().is("@RefID", e.at("@RefID"))) {
+                    fmt.print(":: Removendo {}", item.get().at("@RefID"));
+
+                  //  item.get().at("@Removido", Tronarko.getTronAgora().getTextoZerado());
+
+                  //  item.atualizar();
+                    item.remover();
+                    break;
+                }
+            }
+
+        }
+
+        if(excluidos.getQuantidade()>0){
+            Entidade log = new Entidade();
+            log.at("Tron",Tronarko.getTronAgora().getTextoZerado());
+            log.at("Status","Removendo");
+            log.at("Quantidade",ENTT.CONTAGEM(novos));
+            zeta_logs.adicionar(log);
+        }
+
+        fmt.print(">> OP : SIMETRIA :: EM COMUM");
+        Lista<Par<Entidade, Entidade>> em_comum = ENTT.ANALISAR_OBTER_EM_COMUM_PARES(dados_primarios, dados_secundarios, "@ID", "@RefID");
+
+        fmt.print("Quantidade : {}", em_comum.getQuantidade());
+
+        fmt.print(">> OP : SIMETRIA :: ATUALIZADOS");
+
+        Lista<Par<Entidade, Entidade>> editados = new Lista<Par<Entidade, Entidade>>();
+
+        for (Par<Entidade, Entidade> dupla : em_comum) {
+
+            Lista<String> alpha_atributos = ENTT.GET_ATRIBUTOS_NOMES(dupla.getChave());
+            Lista<String> beta_atributos = ENTT.GET_ATRIBUTOS_NOMES(dupla.getValor());
+
+            alpha_atributos = Strings.RETIRAR_ITEM_SE_COMECAR_COM(alpha_atributos, "@");
+            beta_atributos = Strings.RETIRAR_ITEM_SE_COMECAR_COM(beta_atributos, "@");
+
+            if(Strings.LISTAS_IGUAIS(alpha_atributos,beta_atributos) ){
+
+                boolean editado = false;
+
+                for(String att : alpha_atributos){
+                    if(Strings.isDiferente(dupla.getChave().at(att),dupla.getValor().at(att))){
+                        editado=true;
+                        break;
+                    }
+                }
+
+                if(editado){
+                    editados.adicionar(dupla);
+                }
+
+            }else{
+                editados.adicionar(dupla);
+            }
+
+        }
+
+        fmt.print("Quantidade : {}", editados.getQuantidade());
+
+
+        for (Par<Entidade, Entidade> dupla : editados) {
+
+            ENTT.EXIBIR_TABELA(ENTT.CRIAR_LISTA_COM(dupla.getChave(),dupla.getValor()));
+
+            for (ItemColecionavel item : itens_atualizaveis) {
+                if (!item.isRemovido() && item.get().is("@RefID", dupla.getValor().at("@RefID"))) {
+                    fmt.print(":: Atualizando {}", item.get().at("@RefID"));
+
+                    Lista<String> beta_atributos = ENTT.GET_ATRIBUTOS_NOMES(dupla.getValor());
+                    beta_atributos = Strings.RETIRAR_ITEM_SE_COMECAR_COM(beta_atributos, "@");
+
+                    for(String att : beta_atributos) {
+                        item.get().at_remover(att);
+                    }
+
+                    item.get().at("@Atualizado", Tronarko.getTronAgora().getTextoZerado());
+
+
+                    Lista<String> alpha_atributos = ENTT.GET_ATRIBUTOS_NOMES(dupla.getChave());
+                    alpha_atributos = Strings.RETIRAR_ITEM_SE_COMECAR_COM(alpha_atributos, "@");
+
+                    for(String att : alpha_atributos){
+                        item.get().at(att,dupla.getChave().at(att));
+                    }
+
+                    item.atualizar();
+                    break;
+                }
+            }
+
+        }
+
+        if(editados.getQuantidade()>0){
+            Entidade log = new Entidade();
+            log.at("Tron",Tronarko.getTronAgora().getTextoZerado());
+            log.at("Status","Atualizando");
+            log.at("Quantidade",editados.getQuantidade());
+
+            zeta_logs.adicionar(log);
+        }
+
+        alpha.exibir_colecao();
+        beta.exibir_colecao();
+
+        fmt.print("++ Alpha Contagem : {}", alpha.contagem());
+        fmt.print("++ Beta  Contagem : {}", beta.contagem());
+
+        zeta_logs.exibir_colecao();
+
+        zeta.fechar();
+    }
 
 }
