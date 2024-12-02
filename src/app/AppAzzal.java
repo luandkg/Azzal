6f41858f8d60db531d20ab70aa9b30c44e09c625
Luan Freitas <luandkg@gmail.com>
2024-12-02 10:15:58 -0300

    :hammer: Mudança da localização de pacote da função main da aplicação Azzal (App)

---
+++ src/app/AppAzzal.java
@@ -0,0 +1,346 @@
+package app;
+
+import apps.app_azzal.VamosCalcular;
+import libs.aqz.AQZ;
+import libs.luan.Seq;
+import libs.luan.fmt;
+import libs.tronarko.Tronarko;
+import libs.verkuz.VerkuzImplementador;
+import projetos.aoc2024.AOC_2024;
+
+public class AppAzzal {
+
+    public static void main(String[] args) {
+
+        fmt.print("Tronarko : {}", Tronarko.getTronAgora().getTextoZerado());
+
+        AOC_2024.PROBLEMA(2,AOC_2024.PARTE_2);
+
+        // AzzalUnico.unico("AppAlpha", 1600, 1020, new Alpha());
+
+        // AzzalUnico.unico("AppAzzal", 1600, 1020, new AppGlobal());
+
+        // AzzalUnico.unico("apps.AppFuzz", 1600, 1020, new apps.app_fuzz.AppFuzz());
+
+      //  AppTronarkos.TRONARKO();
+      //  AppTronarkos.ALARME();
+      //  AppTronarkos.ASTROS();
+
+
+        // AzzalUnico.unico("Attuz", 3000, 1000, new AppAttuz());
+        // AzzalUnico.unico("Citatte", 2000, 1000, new AppCitatte());
+
+        //  AzzalUnico.unico("Citatte Modelum", 2000, 1000, new AppCitatteModelum());
+
+        // CidadeGeradorAleatorio.init_cidades();
+        // CidadeGeradorAleatorio.init_cidade();
+        // CidadeGeradorAleatorio.render_cidade_entre_vias("1");
+        // CidadeGeradorAleatorio.render_cidade_entre_vias("2");
+        // CidadeGeradorAleatorio.render_cidade_entre_vias("3");
+
+        // CidadeGeradorAleatorio.render_cidade_entre_vias("melhor_de_3");
+        // CidadeGeradorAleatorio.render_cidade_entre_vias("melhor_de_100");
+
+        // app_testes.init();
+
+        // CidadeGeradorAleatorio.render_cidade();
+        // CidadeGeradorAleatorio.render_gama();
+
+        // AppAttuzServittos.init();
+
+        // AzzalUnico.unico("Linha Do Tempo", 900, 1000, new AppLinhaDoTempo());
+
+        // AzzalUnico.unico("apps.AppAzzal", 1500, 1020, new Alpha());
+        // AzzalUnico.unico("apps.AppAudio", 700, 1020, new AppAudio());
+
+        // AzzalUnico.unico("apps.AppAzzal", 1500, 1020, new C1());
+        // AzzalUnico.unico("apps.AppAzzal", 1500, 1020, new CenaBanco());
+        // AzzalUnico.unico("Quadrante Espacial", 1500, 1010, new QuadranteEspacial());
+
+        // AzzalUnico.unico("apps.AppAzzal", 1100, 800, new Fisica.Fisica());
+
+         //AzzalUnico.unico("Letras", 1300, 1000, new Letras());
+
+        // AzzalUnico.unico("apps.AppAzzal", 1300, 1000, new C1());
+        // AzzalUnico.unico("apps.AppAzzal", 1300, 1000, new Chiado());
+        // AzzalUnico.unico("Cidade", 1300, 1000, new CidadeCena());
+
+        // AzzalUnico.unico("Arch", 1300, 1000, new AppArch());
+
+        // Assembler mAssembler = new Assembler();
+        // mAssembler.compilar("res/montagem.l1", "res/montagem.l0", "res/montagem.o");
+
+        // AzzalUnico.unico("", 1100, 900, new TerrariaCena());
+
+        // AzzalUnico.unico("", 1100, 900, new FonteGeradorCena());
+
+        // AzzalUnico.unico("", 1100, 900, new CenaLetrador());
+
+        // TG22.init();
+
+        // AzzalUnico.unico("Editor - luan.dkg", 1000, 1000, new AppKrhonos());
+
+        // AppGamaFS.init();
+
+        // Servittor.onServico("Arquivador", new ArquivosServicos());
+
+        // AzzalUnico.unico("Visualizador IM", 1500, 1020, new AppImagem());
+        // AzzalUnico.unico("apps.AppAzzal", 1500, 1020, new AppAlbumDeImagens());
+
+        //  AzzalUnico.unico("AppAnimacao", 1100, 900, new AppAnimacao());
+
+        // VideoSequenciador.criar("/home/luan/Vídeos/vi/ecossistema_01.vi", 800, 801,
+        // "/home/luan/Imagens/ecossistema_01/S", 0, 97, ".png");
+        // VideoSequenciador.criar("/home/luan/Vídeos/vi/ecossistema_02.vi", 800, 801,
+        // "/home/luan/Imagens/ecossistema_02/S", 0, 365, ".png");
+        // VideoSequenciador.criar("/home/luan/Vídeos/vi/alunos_v2.vi",3000,2700,"/home/luan/Imagens/alunos_vi/S",
+        // 0, 99, ".png");
+
+        // VideoCodecador.abrir("/home/luan/Vídeos/vi/ecossistema_01.vi");
+
+        // AzzalUnico.unico("AppVideo", 2000, 1100, new AppVideo());
+
+        String ESCOLA_LOCAL = "/home/luan/Dropbox/CED 1";
+
+        // Documentar.organizar(ESCOLA_LOCAL + "/Planejamento/planejamento_01.txt",
+        // ESCOLA_LOCAL + "/Planejamento/PLANEJAMENTO - 1 SEMESTRE - PROFESSOR LUAN
+        // FREITAS.pdf");
+        // Documentar.organizar(ESCOLA_LOCAL + "/Planejamento/planejamento_02.txt",
+        // ESCOLA_LOCAL + "/Planejamento/PLANEJAMENTO - 2 SEMESTRE - PROFESSOR LUAN
+        // FREITAS.pdf");
+
+        // planejamento.organizar("/home/luan/Dropbox/CED_01/Planejamento/pd3_8.txt",
+        // "/home/luan/Dropbox/CED_01/Planejamento/PLANEJAMENTO - PROFº ELVES_PROFª.
+        // IARA_PROFº LUAN -PLANEJAMENTO DE CURSO 2022.pdf");
+
+        // libs.RhoBenchmark.libs.RhoBenchmark.organizar("res/libs.RhoBenchmark.dkg","/home/luan/Imagens/libs.RhoBenchmark.png");
+
+        VamosCalcular vc = new VamosCalcular();
+        // vc.init();
+
+        // libs.Tronarko.Testes.init();
+
+        // AzzalUnico.unico("App_LLCripto", 1100, 900, new App_LLCripto());
+
+        VerkuzImplementador vi = new VerkuzImplementador();
+
+        vi.init("/home/luan/dev/azzal/src/libs");
+        vi.init("/home/luan/dev/azzal/src/libs/azzal");
+        vi.init("/home/luan/dev/azzal/src/libs/mockui");
+
+        //  vi.init_bibliotecas("/home/luan/dev/azzal/src/libs");
+        // vi.exibir();
+
+        // vi.toClasse("/home/luan/IdeaProjects/Azzal/src/azzal_version/AzzalVersion.java","azzal_version","AzzalVersion");
+
+        // AzzalUnico.unico("Corretor", 1100, 900, new AppCorretor());
+
+        // AppMomentum.init();
+        // HarrempluzCreator.criar();
+        // HarrempluzCreator.visualizar();
+
+        // GGDNA.init();
+
+        // AppClassificador.init();
+
+
+        // AppFit.init();
+
+        // GGADF2023.init();
+
+        //  TesteAZ.teste_colecoes();
+
+        // TesteAZ.ver_tozterum();
+
+        // HarrempluzCreator.criar();
+
+        // TesteAQZVolumes.init();
+        // TesteAQZTabelas.init();
+        // TesteAQZTabelas.adicionar_dados();
+
+        //   tempo_descritores();
+        // tempo();
+        // TesteAZ.teste_valorantes();
+        //TesteAZ.teste_valorantes_2();
+        // TesteAZ.teste_tempo();
+        // TesteAZ.teste_tempo_descritores();
+
+        //  tozterum();
+        //TesteAZ.teste_metropoles();
+
+        // MigrarTozterum.ver_inmet();
+        //  MigrarTozterum.realizar_transferencias();
+
+        // SlimeReadMangas.procurar_saint_seiya();
+        // ENTT.EXIBIR_TABELA(INMET.GET_ALERTAR_CLIMATICOS_V2());
+        // AppFerias.recesso_2024_janeiro();
+
+        //aqz_geral();
+
+        // sequenciador();
+
+        // Projettum.init_geral();
+
+        // AzzalUnico.unico("", 1540, 900, new apps.app_workum.AppWorkum());
+
+        // AppCampeonatum.init();
+
+        // FITParser.init();
+
+
+
+        //  TesteFazendario.teste_2();
+          //  TesteFazendario.teste_objetos_grandes();
+
+    //   TesteZettaQuorum.init_grande_estresse();
+
+      //  TesteZettaQuorum.init_fts();
+
+       // TesteZettaQuorum.init_paginar();
+
+      //  TesteZettaPastas.init_pastas();
+
+       // TesteZettaQuorum.init_replicacao();
+
+      //  TesteZettaTabela.ver_dados();
+
+      // TesteZettaQuorum.ver_estrutura();
+
+       // TesteZettaColecaoMonitorada.init_grande_estresse();
+
+        // AtzumProcessoCriativoEmTarefas.EXIBIR_PROCESSO();
+
+        // LimonTorrents.INIT();
+
+        // ServicoExportarTronarko.EXPORTAR_ATZUM();
+        // ServicoExportarTronarko.VER_ATZUM();
+
+        // ServicoTronarko.VIDEO_PLAYER();
+
+        // AnimadoresDeSensores.TRONARKO_VER_SENSORES_FATORES_CLIMATICOS();
+
+        // AppAtzum.INICIAR();
+
+
+       //  TronarkoRenderizadorSensores.TRONARKO_VER_SENSORES_FATORES_CLIMATICOS();
+
+        // AtzumCentralDados.PROXIMIDADE_COM_OCENAO();
+
+       //  ServicoExportarTronarko.EXPORTAR_TRONARKO();
+
+        //  String arquivo_tronarko = AtzumCreator.LOCAL_GET_ARQUIVO("tronarkos/atzum_tronarko_7000.ds");
+        //  DS.DUMP_TABELA(arquivo_tronarko);
+
+        //  ENTT.EXIBIR_TABELA(ENTT.SLICE_PRIMEIROS(dados,10));
+
+
+        //  TesteArquivoDS.alterar_ultimo();
+        //  TesteArquivoDS.alterar_ultimo_dm();
+
+         //Apps.INIT(Apps.PLACAS_TECTONICAS);
+
+        //ServicoTectonico.INIT();
+        // ServicoTectonico.AJUSTAR();
+        // ServicoTectonico.EXTRAIR_PLACAS_TECTONICAS_CONTORNOS();
+        // ServicoTectonico.CRIAR_PLACAS_EXPANDIDAS();
+
+        //  ServicoTectonico.RASTERIZAR();
+        //ServicoTectonico.PLACA_AREA();
+
+      //  AtzumProcessoCriativoEmTarefas.INIT(200);
+
+
+       // AtzumProcessoCriativoEmTarefas.EXIBIR_EXECUTANDO();
+
+
+     //  AztumTronarkoAnalises.VER();
+
+        //  Palkum.init();
+
+        // AtzumProcessoCriativoEmTarefas.INIT_BETA_SEQUENCIAL();
+
+        //   AnalisadorEstruturado. SENSORES_DADOS_ORGANIZAR();
+        // AnalisadorEstruturado.COMPACTAR_SENSORES_SUPERARKO();
+
+        // ServicoExportarTronarko.EXPORTAR_SENSORES_SUPERARKO();
+        // ServicoExportarTronarko.EXPORTAR_MODELOS();
+        //  ServicoExportarTronarko.EXPORTAR_DADOS_CIDADES();
+        //  ServicoExportarTronarko.CONSOLIDAR_DADOS_CIDADES();
+        //   ServicoExportarTronarko.VER_DADOS_VEGETACAO();
+        // ServicoExportarTronarko.VER_DADOS_CIDADES();
+
+        //  AnalisadorEstruturado.VER_DADOS_V9();
+
+        //ServicoTectonico.INIT();
+        //TesteComQTTCacheVer.init();
+
+        // JujutsuKaizen.init();
+
+        //  PessoaNomeadorDeAkkax.VISUALIZAR_AMOSTRA_PEQUENA();
+
+        // AppStrava.init();
+
+        // Teste.init();
+
+        // tempo();
+
+        // Cartaze.init();
+
+        // TronarkoOrganizarAssets.signos();
+
+        //AppCartaze.INIT();
+
+    }
+
+
+    public static void tozterum() {
+
+        String arquivo_banco = "/home/luan/assets/TozterumBackup.az";
+
+        // AQZ.EXIBIR_ESTRUTURA(arquivo_banco);
+
+
+        // AQZUTF8.EXIBIR_AMOSTRA(arquivo_banco, "STRAVA_ACOMPANHAMENTO_DADOS(LL)");
+        //  AQZUTF8.EXIBIR_CONTEUDO(arquivo_banco, "STRAVA_ACOMPANHAMENTO_DADOS(LL)");
+        //  AQZUTF8.EXIBIR_DESCRITORES(arquivo_banco, "STRAVA_ACOMPANHAMENTO_DADOS(LL)");
+
+        //   AQZ.EXIBIR_DISPERSAO(arquivo_banco,"INMET.DADOS","Data");
+
+        String arquivo_banco_inmet = "/home/luan/assets/INMET.az";
+        AQZ.EXIBIR_ESTRUTURA_PUBLICA_DETALHADA(arquivo_banco_inmet);
+        //AQZ.EXIBIR_COLECAO(arquivo_banco_inmet, "ALERTAS_INMET");
+        // AQZ.EXIBIR_COLECAO_TAMANHO(arquivo_banco_inmet, "ALERTAS_INMET");
+
+    }
+
+
+    public static void sequenciador() {
+
+        for (Integer indice : Seq.SEQUENCIE(0, 100)) {
+            fmt.print("{}", indice);
+        }
+
+    }
+
+
+    public static void banco_testes() {
+
+        String arquivo_banco = "/home/luan/assets/max.aqz";
+
+        for (int id = 0; id <= 10000; id++) {
+            // AQZ.COLECOES_ORGANIZAR(arquivo_banco, "Tronakum_"+id);
+            // fmt.print("Banco :: "+"Tronakum_"+id);
+        }
+
+        AQZ.AUTO_ANALISAR(arquivo_banco);
+        AQZ.ANALISAR(arquivo_banco);
+
+        AQZ.EXIBIR_COLECAO_PRIMARIA(arquivo_banco, "@Init");
+        AQZ.EXIBIR_COLECAO_PRIMARIA(arquivo_banco, "@Bancos");
+        AQZ.EXIBIR_COLECAO_PRIMARIA(arquivo_banco, "@Sequencias");
+        AQZ.EXIBIR_COLECAO_PRIMARIA(arquivo_banco, "@AutoAnalise");
+        AQZ.EXIBIR_COLECAO_PRIMARIA(arquivo_banco, "@Analise");
+
+    }
+
+}
