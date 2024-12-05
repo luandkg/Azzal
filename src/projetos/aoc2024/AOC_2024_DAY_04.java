ec4cb4ee71ad9a9e838ab3908f7c02534435f498
Luan Freitas <luandkg@gmail.com>
2024-12-04 23:30:33 -0300

    :hammer: Utilizando tabuleiro em AOC_2024_DAY_04

--- src/projetos/aoc2024/AOC_2024_DAY_04.java
+++ src/projetos/aoc2024/AOC_2024_DAY_04.java
@@ -1,7 +1,7 @@
 package projetos.aoc2024;
 
-import libs.luan.Lista;
 import libs.luan.Strings;
+import libs.luan.Tabuleiro;
 import libs.luan.Texto;
 import libs.luan.fmt;
 
@@ -9,168 +9,6 @@ public class AOC_2024_DAY_04 extends AOC_2024_DAY {
 
     public AOC_2024_DAY_04() {
         super(4, "Ceres Search");
-    }
-
-    class Letra {
-
-        public int linha = 0;
-        public int coluna = 0;
-        public String letra = "";
-        private boolean descoberto = false;
-
-        public Letra(int eLinha, int eColuna, String eLetra) {
-            linha = eLinha;
-            coluna = eColuna;
-            letra = eLetra;
-            descoberto = false;
-        }
-
-    }
-
-    class Tabuleiro {
-
-        public Lista<Lista<Letra>> mLinhas = new Lista<Lista<Letra>>();
-
-
-        public String getLetra(int linha, int coluna) {
-            return mLinhas.get(linha).get(coluna).letra;
-        }
-
-        public void marque_letra(int linha, int coluna, boolean marcado) {
-            mLinhas.get(linha).get(coluna).descoberto = marcado;
-        }
-
-        public void criar_linha() {
-            mLinhas.adicionar(new Lista<Letra>());
-        }
-
-        public void adicionar_letra(String letra) {
-            mLinhas.getUltimoValor().adicionar(new Letra(mLinhas.getQuantidade(), mLinhas.getUltimoValor().getQuantidade(), letra));
-        }
-
-        public void exibir() {
-            for (Lista<Letra> linha : mLinhas) {
-                String s_linha = "";
-                for (Letra letra : linha) {
-                    s_linha += letra.letra + " ";
-                }
-                fmt.print("{}", s_linha);
-            }
-        }
-
-        public void exibir_descobertos() {
-            for (Lista<Letra> linha : mLinhas) {
-                String s_linha = "";
-                for (Letra letra : linha) {
-                    if (letra.descoberto) {
-                        s_linha += letra.letra + " ";
-                    } else {
-                        s_linha += "." + " ";
-                    }
-                }
-                fmt.print("{}", s_linha);
-            }
-        }
-
-
-        public int getLinhasQuantidade() {
-            return mLinhas.getQuantidade();
-        }
-
-        public int getLetrasQuantidadeDaLinha(int linha) {
-            return mLinhas.get(linha).getQuantidade();
-        }
-
-        public boolean posicao_valida(int linha, int coluna) {
-            if (linha >= 0 && linha < mLinhas.getQuantidade()) {
-                if (coluna >= 0 && coluna < mLinhas.get(linha).getQuantidade()) {
-                    return true;
-                }
-            }
-            return false;
-        }
-
-        public void marcar_letras_horizontalmente(int linha, int coluna, int quantidade) {
-
-            for (int i = coluna; i < coluna + quantidade; i++) {
-                marque_letra(linha, i, true);
-            }
-
-        }
-
-        public void marcar_letras_verticalmente(int linha, int coluna, int quantidade) {
-
-
-            for (int i = linha; i < linha + quantidade; i++) {
-                marque_letra(i, coluna, true);
-
-            }
-
-        }
-
-
-        public void marque_letras_diagonalmente_abaixo_direita(int linha, int coluna, int quantidade) {
-
-            for (int i = 0; i < quantidade; i++) {
-                marque_letra(linha + i, coluna + i, true);
-
-            }
-
-        }
-
-        public void marque_letras_diagonalmente_abaixo_esquerda(int linha, int coluna, int quantidade) {
-
-            for (int i = 0; i < quantidade; i++) {
-                marque_letra(linha + i, coluna - i, true);
-            }
-
-        }
-
-
-        public String getLetrasHorizontalmente(int linha, int coluna, int quantidade) {
-
-            String ret = "";
-
-            for (int i = coluna; i < coluna + quantidade; i++) {
-                ret += getLetra(linha, i);
-            }
-
-            return ret;
-        }
-
-        public String getLetrasVerticalmente(int linha, int coluna, int quantidade) {
-
-            String ret = "";
-
-            for (int i = linha; i < linha + quantidade; i++) {
-                ret += getLetra(i, coluna);
-            }
-
-            return ret;
-        }
-
-        public String getLetrasDiagonalmenteAbaixoDireita(int linha, int coluna, int quantidade) {
-
-            String ret = "";
-
-            for (int i = 0; i < quantidade; i++) {
-                ret += getLetra(linha + i, coluna + i);
-            }
-
-            return ret;
-        }
-
-        public String getLetrasDiagonalmenteAbaixoEsquerda(int linha, int coluna, int quantidade) {
-
-            String ret = "";
-
-            for (int i = 0; i < quantidade; i++) {
-                ret += getLetra(linha + i, coluna - i);
-            }
-
-            return ret;
-        }
-
     }
 
 
@@ -185,15 +23,11 @@ public class AOC_2024_DAY_04 extends AOC_2024_DAY {
 
         fmt.print(">> Processando");
 
-        Tabuleiro tabuleiro = new Tabuleiro();
+        Tabuleiro<String> tabuleiro = new Tabuleiro<String>();
 
         for (String linha : Strings.DIVIDIR_LINHAS(texto_entrada)) {
 
-            tabuleiro.criar_linha();
-
-            for (String letra : Strings.GET_LETRAS(linha)) {
-                tabuleiro.adicionar_letra(letra);
-            }
+            tabuleiro.adicionar_linha(Strings.GET_LETRAS(linha));
 
         }
 
@@ -203,48 +37,49 @@ public class AOC_2024_DAY_04 extends AOC_2024_DAY {
 
         for (int linha = 0; linha < tabuleiro.getLinhasQuantidade(); linha++) {
             for (int coluna = 0; coluna < tabuleiro.getLetrasQuantidadeDaLinha(linha); coluna++) {
-                String letra = tabuleiro.getLetra(linha, coluna);
+                String letra = tabuleiro.getValor(linha, coluna);
 
                 if (Strings.isIgual(letra, "X")) {
 
                     if (tabuleiro.posicao_valida(linha, coluna + 3)) {
 
-                        String palavra = tabuleiro.getLetrasHorizontalmente(linha, coluna, 4);
+                        String palavra = Tabuleiro.TRANSFORMAR_STRING(tabuleiro.getHorizontalmente(linha, coluna, 4));
                         if (Strings.isIgual(palavra, "XMAS")) {
                             fmt.print("XMAS HORIZONTAL DIRETO  ->> {}:{}", linha, coluna);
                             xmas_contagem += 1;
-                            tabuleiro.marcar_letras_horizontalmente(linha, coluna, 4);
+                            tabuleiro.marcar_horizontalmente(linha, coluna, 4);
                         }
 
                     }
                     if (tabuleiro.posicao_valida(linha + 3, coluna)) {
 
-                        String palavra = tabuleiro.getLetrasVerticalmente(linha, coluna, 4);
+                        String palavra = Tabuleiro.TRANSFORMAR_STRING(tabuleiro.getVerticalmente(linha, coluna, 4));
                         if (Strings.isIgual(palavra, "XMAS")) {
                             fmt.print("XMAS VERTICAL DIRETO    ->> {}:{}", linha, coluna);
                             xmas_contagem += 1;
-                            tabuleiro.marcar_letras_verticalmente(linha, coluna, 4);
+                            tabuleiro.marcar_verticalmente(linha, coluna, 4);
                         }
 
                     }
 
                     if (tabuleiro.posicao_valida(linha + 3, coluna + 3)) {
 
-                        String palavra = tabuleiro.getLetrasDiagonalmenteAbaixoDireita(linha, coluna, 4);
+                        String palavra = Tabuleiro.TRANSFORMAR_STRING(tabuleiro.getDiagonalmenteAbaixoDireita(linha, coluna, 4));
                         if (Strings.isIgual(palavra, "XMAS")) {
                             fmt.print("XMAS DIAGONAL ABAIXO DIREITA DIRETO    ->> {}:{}", linha, coluna);
                             xmas_contagem += 1;
-                            tabuleiro.marque_letras_diagonalmente_abaixo_direita(linha, coluna, 4);
+                            tabuleiro.marque_diagonalmente_abaixo_direita(linha, coluna, 4);
                         }
 
                     }
 
                     if (tabuleiro.posicao_valida(linha + 3, coluna - 3)) {
 
-                        String palavra = tabuleiro.getLetrasDiagonalmenteAbaixoEsquerda(linha, coluna, 4);
+                        String palavra = Tabuleiro.TRANSFORMAR_STRING(tabuleiro.getDiagonalmenteAbaixoEsquerda(linha, coluna, 4));
                         if (Strings.isIgual(palavra, "XMAS")) {
                             fmt.print("XMAS DIAGONAL ABAIXO ESQUERDA DIRETO    ->> {}:{}", linha, coluna);
                             xmas_contagem += 1;
+                            tabuleiro.marque_diagonalmente_abaixo_esquerda(linha, coluna, 4);
                         }
 
                     }
@@ -254,12 +89,12 @@ public class AOC_2024_DAY_04 extends AOC_2024_DAY {
 
                     if (tabuleiro.posicao_valida(linha, coluna + 3)) {
 
-                        String palavra = Strings.REVERSE(tabuleiro.getLetrasHorizontalmente(linha, coluna, 4));
+                        String palavra = Strings.REVERSE(Tabuleiro.TRANSFORMAR_STRING(tabuleiro.getHorizontalmente(linha, coluna, 4)));
 
                         if (Strings.isIgual(palavra, "XMAS")) {
                             fmt.print("XMAS HORIZONTAL REVERSO ->> {}:{}", linha, coluna);
                             xmas_contagem += 1;
-                            tabuleiro.marcar_letras_horizontalmente(linha, coluna, 4);
+                            tabuleiro.marcar_horizontalmente(linha, coluna, 4);
 
                         }
 
@@ -267,33 +102,33 @@ public class AOC_2024_DAY_04 extends AOC_2024_DAY {
 
                     if (tabuleiro.posicao_valida(linha + 3, coluna)) {
 
-                        String palavra = Strings.REVERSE(tabuleiro.getLetrasVerticalmente(linha, coluna, 4));
+                        String palavra = Strings.REVERSE(Tabuleiro.TRANSFORMAR_STRING(tabuleiro.getVerticalmente(linha, coluna, 4)));
                         if (Strings.isIgual(palavra, "XMAS")) {
                             fmt.print("XMAS VERTICAL REVERSO    ->> {}:{}", linha, coluna);
                             xmas_contagem += 1;
-                            tabuleiro.marcar_letras_verticalmente(linha, coluna, 4);
+                            tabuleiro.marcar_verticalmente(linha, coluna, 4);
                         }
                     }
 
 
                     if (tabuleiro.posicao_valida(linha + 3, coluna + 3)) {
 
-                        String palavra = Strings.REVERSE(tabuleiro.getLetrasDiagonalmenteAbaixoDireita(linha, coluna, 4));
+                        String palavra = Strings.REVERSE(Tabuleiro.TRANSFORMAR_STRING(tabuleiro.getDiagonalmenteAbaixoDireita(linha, coluna, 4)));
                         if (Strings.isIgual(palavra, "XMAS")) {
                             fmt.print("XMAS DIAGONAL ABAIXO DIREITA REVERSO    ->> {}:{}", linha, coluna);
                             xmas_contagem += 1;
-                            tabuleiro.marque_letras_diagonalmente_abaixo_direita(linha, coluna, 4);
+                            tabuleiro.marque_diagonalmente_abaixo_direita(linha, coluna, 4);
                         }
 
                     }
 
                     if (tabuleiro.posicao_valida(linha + 3, coluna - 3)) {
 
-                        String palavra = Strings.REVERSE(tabuleiro.getLetrasDiagonalmenteAbaixoEsquerda(linha, coluna, 4));
+                        String palavra = Strings.REVERSE(Tabuleiro.TRANSFORMAR_STRING(tabuleiro.getDiagonalmenteAbaixoEsquerda(linha, coluna, 4)));
                         if (Strings.isIgual(palavra, "XMAS")) {
                             fmt.print("XMAS DIAGONAL ABAIXO ESQUERDA DIRETO    ->> {}:{}", linha, coluna);
                             xmas_contagem += 1;
-                            tabuleiro.marque_letras_diagonalmente_abaixo_esquerda(linha, coluna, 4);
+                            tabuleiro.marque_diagonalmente_abaixo_esquerda(linha, coluna, 4);
                         }
 
                     }
@@ -322,7 +157,7 @@ public class AOC_2024_DAY_04 extends AOC_2024_DAY {
 
         fmt.print(">> Processando");
 
-        Tabuleiro tabuleiro = new Tabuleiro();
+        Tabuleiro<String> tabuleiro = new Tabuleiro<String>();
 
         for (String linha : Strings.DIVIDIR_LINHAS(texto_entrada)) {
 
@@ -344,57 +179,56 @@ public class AOC_2024_DAY_04 extends AOC_2024_DAY {
         for (int linha = 0; linha < tabuleiro.getLinhasQuantidade(); linha++) {
 
             for (int coluna = 0; coluna < tabuleiro.getLetrasQuantidadeDaLinha(linha); coluna++) {
-                String letra = tabuleiro.getLetra(linha, coluna);
+                String letra = tabuleiro.getValor(linha, coluna);
 
-                if (Strings.isIgual(letra, "A") ) {
+                if (Strings.isIgual(letra, "A")) {
 
 
-                    int d_linha = linha-1;
-                    int d_coluna=coluna-1;
+                    int d_linha = linha - 1;
+                    int d_coluna = coluna - 1;
 
-                    int b_linha = linha-1;
-                    int b_coluna=coluna+1;
+                    int b_linha = linha - 1;
+                    int b_coluna = coluna + 1;
 
                     boolean lado_a = false;
-                    boolean lado_b=false;
+                    boolean lado_b = false;
 
 
+                    fmt.print("AA : {}::{}", linha, coluna);
 
-                    fmt.print("AA : {}::{}",linha,coluna);
+                    if (tabuleiro.posicao_valida(d_linha, d_coluna) && tabuleiro.posicao_valida(d_linha + 2, d_coluna + 2)) {
 
-                    if (tabuleiro.posicao_valida(d_linha, d_coluna) && tabuleiro.posicao_valida(d_linha+2, d_coluna+2)) {
-
-                        String palavra = tabuleiro.getLetrasDiagonalmenteAbaixoDireita(d_linha, d_coluna, 3);
+                        String palavra = Tabuleiro.TRANSFORMAR_STRING(tabuleiro.getDiagonalmenteAbaixoDireita(d_linha, d_coluna, 3));
                         //fmt.print("{}:{} -- Enc :: {}",linha,coluna,palavra);
 
-                        fmt.print("Aqui D : {}::{}",b_linha,b_coluna);
+                        fmt.print("Aqui D : {}::{}", b_linha, b_coluna);
 
-                        if (Strings.isIgual(letra, "A") && (Strings.isIgual(palavra, PALAVRA_CHAVE)||Strings.isIgual(palavra, PALAVRA_CHAVE_REVERSA) ) ) {
+                        if (Strings.isIgual(letra, "A") && (Strings.isIgual(palavra, PALAVRA_CHAVE) || Strings.isIgual(palavra, PALAVRA_CHAVE_REVERSA))) {
                             fmt.print("MAS DIAGONAL ABAIXO DIREITA     ->> {}:{}", d_linha, d_coluna);
                             lado_a = true;
                         }
 
                     }
 
-                    if (tabuleiro.posicao_valida(b_linha, b_coluna) && tabuleiro.posicao_valida(b_linha+2, b_coluna-2)) {
+                    if (tabuleiro.posicao_valida(b_linha, b_coluna) && tabuleiro.posicao_valida(b_linha + 2, b_coluna - 2)) {
 
-                        fmt.print("Aqui E : {}::{}",b_linha,b_coluna);
-                        String palavra = tabuleiro.getLetrasDiagonalmenteAbaixoEsquerda(b_linha, b_coluna, 3);
-                        fmt.print("{}:{} -- Enc ED :: {}",linha,coluna,palavra);
+                        fmt.print("Aqui E : {}::{}", b_linha, b_coluna);
+                        String palavra = Tabuleiro.TRANSFORMAR_STRING(tabuleiro.getDiagonalmenteAbaixoEsquerda(b_linha, b_coluna, 3));
+                        fmt.print("{}:{} -- Enc ED :: {}", linha, coluna, palavra);
 
 
-                        if (Strings.isIgual(letra, "A") && (Strings.isIgual(palavra, PALAVRA_CHAVE)||Strings.isIgual(palavra, PALAVRA_CHAVE_REVERSA) ) ) {
+                        if (Strings.isIgual(letra, "A") && (Strings.isIgual(palavra, PALAVRA_CHAVE) || Strings.isIgual(palavra, PALAVRA_CHAVE_REVERSA))) {
                             fmt.print("MAS DIAGONAL ABAIXO ESQUERDA    ->> {}:{}", b_linha, b_coluna);
                             lado_b = true;
                         }
 
                     }
 
-                    fmt.print("A ->> {}::{} com {}:{}",linha,coluna,lado_a,lado_b);
+                    fmt.print("A ->> {}::{} com {}:{}", linha, coluna, lado_a, lado_b);
 
-                    if(lado_a && lado_b ) {
-                        tabuleiro.marque_letras_diagonalmente_abaixo_direita(d_linha, d_coluna, 3);
-                        tabuleiro.marque_letras_diagonalmente_abaixo_esquerda(b_linha, b_coluna, 3);
+                    if (lado_a && lado_b) {
+                        tabuleiro.marque_diagonalmente_abaixo_direita(d_linha, d_coluna, 3);
+                        tabuleiro.marque_diagonalmente_abaixo_esquerda(b_linha, b_coluna, 3);
                         xmas_contagem += 1;
                     }
 
