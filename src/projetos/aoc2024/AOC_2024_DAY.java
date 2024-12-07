d5ff7f2fd04159dc6d9e8119cb79dc80f1de937c
Luan Freitas <luandkg@gmail.com>
2024-12-07 12:14:54 -0300

    :hammer: Advent Of Code 2024 - adicionar funções de log info

--- src/projetos/aoc2024/AOC_2024_DAY.java
+++ src/projetos/aoc2024/AOC_2024_DAY.java
@@ -2,35 +2,46 @@ package projetos.aoc2024;
 
 import libs.luan.fmt;
 
-public abstract  class AOC_2024_DAY {
+public abstract class AOC_2024_DAY {
 
 
     private int mProblemaNumero;
     private String mProblemaNome;
 
-    public AOC_2024_DAY(  int eProblemaNumero,String eNome){
-        mProblemaNumero=eProblemaNumero;
-        mProblemaNome=eNome;
+    public AOC_2024_DAY(int eProblemaNumero, String eNome) {
+        mProblemaNumero = eProblemaNumero;
+        mProblemaNome = eNome;
     }
 
-    public int getProblemaNumero(){
+    public int getProblemaNumero() {
         return mProblemaNumero;
     }
 
-    public String getProblemaNome(){
+    public String getProblemaNome() {
         return mProblemaNome;
     }
 
     public abstract void parte_1();
+
     public abstract void parte_2();
 
 
-    public String getArquivoDica(){
-        return AOC_2024.GET_ARQUIVO("TIP_"+ fmt.zerado(mProblemaNumero,2)+".txt");
+    public String getArquivoDica() {
+        return AOC_2024.GET_ARQUIVO("TIP_" + fmt.zerado(mProblemaNumero, 2) + ".txt");
     }
 
-    public String getArquivoEntrada(){
-        return AOC_2024.GET_ARQUIVO("DAY_"+ fmt.zerado(mProblemaNumero,2)+".txt");
+    public String getArquivoEntrada() {
+        return AOC_2024.GET_ARQUIVO("DAY_" + fmt.zerado(mProblemaNumero, 2) + ".txt");
+    }
+
+
+
+    public void info(int parte, String mensagem) {
+        AOC_2024.INFO(getProblemaNumero(), parte, mensagem);
+    }
+
+    public void info(int parte, int mensagem) {
+        AOC_2024.INFO(getProblemaNumero(), parte, String.valueOf(mensagem));
     }
 
 }
